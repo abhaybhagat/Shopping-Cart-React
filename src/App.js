52485f1b4b63adb5cda9ff6f10ba94a5ad7e4d71
abhaybhagat <bhagatabhay80@gmail.com>
2020-08-13 16:33:42 +0530

    changed add, delete, reduce quantity buttons to directly affect firebase backend and not just react state

--- src/App.js
+++ src/App.js
@@ -1,46 +1,60 @@
 import React from "react";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: []
-    };  
+      products: [],
+    };
   }
-  
-componentDidMount() {
-  firebase
-  .firestore()
-  .collection('products')
-  .get()
-  .then((snapshot) => {
-    console.log(snapshot);
-    snapshot.docs.map((doc)=> {
-      console.log(doc.data());
-    })
-    const products = snapshot.docs.map((doc) => {
-      const data = doc.data();
-      data['id'] = doc.id;
-      return data;
-    })
-    this.setState({
-      products : products
-    })
-  })
-}
+
+  componentDidMount() {
+    firebase
+      .firestore()
+      .collection("products")
+      .onSnapshot((snapshot) => {
+        console.log(snapshot);
+        snapshot.docs.map((doc) => {
+          console.log(doc.data());
+        });
+        const products = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          data["id"] = doc.id;
+          return data;
+        });
+        this.setState({
+          products: products,
+        });
+      });
+  }
 
   handleIncreaseQuantity = (product) => {
     console.log("Increase qty of ", product);
     const { products } = this.state;
     const index = products.indexOf(product);
-    products[index].qty += 1;
+    // products[index].qty += 1;
 
-    this.setState({
-      products: products,
-    });
+    // this.setState({
+    //   products: products,
+    // });
+
+    const docRef = firebase
+      .firestore()
+      .collection("products")
+      .doc(products[index].id);
+    docRef
+      .update({
+        qty: products[index].qty + 1,
+      })
+      .then(() => {
+        console.log("Update Success");
+      })
+      .catch(() => {
+        console.log("Update failed");
+      });
   };
   handleDecreaseQuantity = (product) => {
     console.log("Decrease qty of ", product);
@@ -49,11 +63,25 @@ componentDidMount() {
     if (products[index].qty === 0) {
       return;
     }
-    products[index].qty -= 1;
+    // products[index].qty -= 1;
 
-    this.setState({
-      products: products,
-    });
+    // this.setState({
+    //   products: products,
+    // });
+    const docRef = firebase
+      .firestore()
+      .collection("products")
+      .doc(products[index].id);
+    docRef
+      .update({
+        qty: products[index].qty - 1,
+      })
+      .then(() => {
+        console.log("Update Success");
+      })
+      .catch(() => {
+        console.log("Update failed");
+      });
   };
   getCartCount = () => {
     const { products } = this.state;
@@ -65,22 +93,34 @@ componentDidMount() {
   };
   handleDeleteQuantity = (id) => {
     const { products } = this.state;
-    const items = products.filter((item) => item.id !== id);
-    this.setState({
-      products: items,
-    });
+    // const items = products.filter((item) => item.id !== id);
+    // this.setState({
+    //   products: items,
+    // });
+    const docRef = firebase
+      .firestore()
+      .collection("products")
+      .doc(id);
+      docRef
+        .delete()
+        .then(() => {
+          console.log("Update Success");
+        })
+        .catch(() => {
+          console.log("Update failed");
+        });
   };
   getCartTotal = () => {
-     const { products } = this.state;
+    const { products } = this.state;
 
-     let cartTotal = 0;
+    let cartTotal = 0;
 
-     products.map((product) => {
-       cartTotal = cartTotal + product.qty * product.price;
-       return '';
-     })
-     return cartTotal;
-  }
+    products.map((product) => {
+      cartTotal = cartTotal + product.qty * product.price;
+      return "";
+    });
+    return cartTotal;
+  };
   render() {
     const { products } = this.state;
     return (
@@ -92,7 +132,10 @@ componentDidMount() {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDelete={this.handleDeleteQuantity}
         />
-        <div style = {{padding : 10,fontSize : 20}}> Total : {this.getCartTotal()}</div>
+        <div style={{ padding: 10, fontSize: 20 }}>
+          {" "}
+          Total : {this.getCartTotal()}
+        </div>
       </div>
     );
   }
